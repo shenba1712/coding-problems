96c6e17b440b56ebaa2d271e9e10007375cedbc4
Shenba <shenbagalakshmi1712@gmail.com>
2016-11-13 12:53:01 +0800

    Update Printer.java

--- Printer.java
+++ Printer.java
@@ -6,7 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Printer_Visenze {
+public class Printer {
 	
 		// taking paper's height as 40
 		private final static int height = 40;
