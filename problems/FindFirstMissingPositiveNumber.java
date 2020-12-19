e2a9032ddcfb767c50f5584b078a777d9eb6408b
shenba1712 <shenbagalakshmi1712@gmail.com>
2020-12-19 23:10:24 +0100

    fix bug when index contains negative value

--- problems/FindFirstMissingPositiveNumber.java
+++ problems/FindFirstMissingPositiveNumber.java
@@ -25,25 +25,28 @@ public class FindFirstMissingPositiveNumber {
         int j = 0;
         for (int i = 0; i < n; i++) {
             if (array[i] <= 0) {
-                int temp = array[i];
+                int temp = -array[i];
                 array[i] = array[j];
                 array[j] = temp;
                 j++;
             }
         }
 
+        // start from the first positive index (j)
         for (int i = j; i < n ; i++) {
             int number = Math.abs(array[i]);
 
             // mark the array[index] visited by marking it as -number
+            // in case of duplicates, the number should still be counted as visited (-ve)
+            // Hence we use Math.abs()
             if (number <= n) {
-                array[number-1] = -array[number-1];
+                array[number-1] = -Math.abs(array[number-1]);
             }
         }
 
         // the index of the first positive number is the missing number
-        for (int i = j; i < n; i++) {
-            if (array[i] > 0) {
+        for (int i = 0; i < n; i++) {
+            if (array[i] >= 0) {
                 System.out.println("First missing number is " + (i+1));
                 break;
             }
